a344e6fa9823c693d7dc42a650d39a22043f012f
gile987 <mladen.reljic87@gmail.com>
2022-09-12 19:50:18 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+import {token} from './token.js';
+
+document
+  .querySelector('button')
+  .addEventListener('click', function () {
+    fetchShortenedUrl();
+  });
+
+const fetchShortenedUrl = async () => {
+  const url = document.querySelector('input').value;
+  const outputUrl = document.getElementById('output__url');
+  if (url && (url.startsWith('http://') || url.startsWith('https://'))) {
+    try {
+      const response = await fetch(`https://api-ssl.bitly.com/v4/shorten`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ long_url: url, domain: 'bit.ly' })
+        });
+      const data = await response.json();
+      if (data.link) {
+        outputUrl.innerHTML = data.link;
+      } else {
+        outputUrl.innerHTML = data.message;
+      }
+    } catch (err) {
+      console.log("ERROR: ", err);
+    };
+  } else {
+    outputUrl.innerHTML = 'Invalid URL';
+  };
+};
